1a5f1bfe01f04dc0dd43d750f1fe9d4152e54179
Trinh253 <trinhtt001@gmail.com>
2023-08-29 20:38:52 -0700

    Th

---
+++ BraveSoftware/Brave-Browser/User Data/Profile 2/Default/Extensions/mafpmfcccpbjnhfhjnllmmalhifmlcie/0.7.2_0/embed.js
@@ -0,0 +1,42 @@
+/* global chrome, Popup */
+
+window.onload = () => {
+  // Fill i18n in HTML
+  Popup.fill(document.body, (m) => {
+    return chrome.i18n.getMessage(m);
+  });
+
+  const port = chrome.runtime.connect({
+    name: "popup"
+  });
+
+  const popup = new Popup(
+    (...args) => chrome.i18n.getMessage(...args),
+    (event) => port.postMessage({ enabled: event.target.checked }),
+    () => port.postMessage({ retry: true }),
+    (
+      (
+        typeof true !== 'undefined'
+        // eslint-disable-next-line no-undef
+        && true
+      )
+        ? (newValue) => port.postMessage({ runInBackground: newValue })
+        : undefined
+    )
+  );
+
+  port.onMessage.addListener((m) => {
+    const { clients, enabled, total, missingFeature } = m;
+
+    if (missingFeature) {
+      popup.missingFeature(missingFeature);
+      return;
+    }
+
+    if (enabled) {
+      popup.turnOn(clients, total);
+    } else {
+      popup.turnOff();
+    }
+  });
+};
